dd1ac1862426743e146b2720bd7103144b86b895
ausias bm <ausiasbm@gmail.com>
2020-02-14 13:59:15 +0100

    mostramos un JSON

---
+++ src/api/v1.0/index.php
@@ -0,0 +1,35 @@
+<?php
+
+$serverNombre = "localhost";
+$userNombre = "root";
+$password = "";
+$dbNombre = "control_ventas";
+
+// Crear la conexión
+$conn = mysqli_connect($serverNombre,
+    $userNombre, $password, $dbNombre);
+
+// Chequear la conexión
+if (!$conn) {
+    die("Error: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM `ventas`";
+$result = mysqli_query($conn, $sql);
+
+$output = array();
+
+if (mysqli_num_rows($result) > 0) {
+    // procesar cada fila
+    while($row = mysqli_fetch_assoc($result)) {
+        //echo $row["fecha"]. '<br>';
+        array_push($output, $row);
+    }
+
+} else {
+    echo "0 resultados";
+}
+
+// recoger el array y lo convierte en una cadena JSON
+header('Content-type: application/json');
+echo json_encode($output);
